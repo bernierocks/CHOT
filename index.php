2d13413e21344fc8e539a7fe2674eaaf405375bc
Bernie Colita <kittywebworks@gmail.com>
2015-08-21 03:29:37 +0800

    cc

--- index.php
+++ index.php
@@ -12,12 +12,10 @@ include('header.php'); // Include pulls another file into the page, and sticks i
                           
                     </div><!-- /.col6-->
                     <div class="col col-sm-12 col-md-6"> 
-                       <h1>Welcome to <b>Church</b>Organ<b>Trader.com</b></h1> 
-                       <div class="row">  
+                       <h1>Welcome to <b>Church</b>Organ<b>Trader.com</b></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
 Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam.
                        </p>
-                       </div><!-- /.row -->
                        <a href="#" class="btn btn-primary btn-large"> Log Into Site</a>
                     </div><!-- /.col6-->
                 </div><!-- /.row -->
