b9f8f61de352f8cad93d6e69150053142a454cfe
Bernie Colita <kittywebworks@gmail.com>
2015-08-21 03:00:39 +0800

    cc

--- index.php
+++ index.php
@@ -3,4 +3,15 @@ $home = 'Y'; // This variable designates this as the index page, and tells the h
 include('header.php'); // Include pulls another file into the page, and sticks it on where the include is called.
 ?>
 
+<section class="bc-1">   
+    <div class="container">  
+        <div class="col col-sm-12 col-md-12">  
+            <div class="hero-bg">
+                
+            </div><!-- /. -->
+        </div><!-- /.col12-->
+    </div><!-- /.container -->
+
+</section><!-- /.section -->
+
 <?include('footer.php'); ?>
