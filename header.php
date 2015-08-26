b03b506a94f10717bb75f987180c461c178706e0
Bernie Colita <kittywebworks@gmail.com>
2015-08-27 04:45:57 +0800

    cc

--- header.php
+++ header.php
@@ -5,14 +5,7 @@
 <link href='//fonts.googleapis.com/css?family=Marcellus|Open+Sans:400,700' rel='stylesheet' type='text/css'>
 
 
-<?
-/* The next section deals with titles on sub pages */
-// Keep in mind that any divs started in this block of code must be closed in the footer
-if($home!='Y' && $_GET['mms'] != 'X'){ //this IF statement is the test.  If these items evaluate to TRUE, it will perform the first block of code.
-	echo '<h2 class="title">'.$title.'</h2>';//ECHO is PHP's command to output to the page.
-} else { //If the IF statement evaluates to FALSE, this code will be run.  This is optional.
 
-}?> 
 
 
 <header>   
