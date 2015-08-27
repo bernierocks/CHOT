c8debf8d7ef5809a94f5d0301cf2beb9f7c8ee1e
Bernie Colita <kittywebworks@gmail.com>
2015-08-28 03:29:37 +0800

    cc

--- header.php
+++ header.php
@@ -61,7 +61,7 @@
         
         <div class="row">  
             <div class="col col-sm-12 col-md-12  hidden-md hidden-lg">   
-                <a href="#" class="btn btn-primary btn-large" data-toggle="modal" data-target="#myModal"> 
+                <a href="#" class="bc-mobilelogin btn btn-primary btn-large" data-toggle="modal" data-target="#myModal"> 
                     Member Login
                 </a>
            <div id="main-menu" class="hidden-xs	hidden-sm">
